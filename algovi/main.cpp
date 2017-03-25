47d8f50d9f3e683ed97e3f80e714ed81bb5de980
Dmytro Sadovyi <velter17@gmail.com>
2017-03-25 20:00:18 +0200

    Initial commit

---
+++ algovi/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * Project  AlgoVi
+ *
+ * @file    main.cpp
+ * @author  Dmytro Sadovyi
+ * @date    25.03.2017
+*/
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello world!\n";
+    return 0;
+}
